a6b4b0342a8c051214c84ee19eb9c3ab600f3b05
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-08 19:49:10 -1000

    Update Start_Here.py
    try 3

--- python/streamlit-multipage-chat-files-citations/Start_Here.py
+++ python/streamlit-multipage-chat-files-citations/Start_Here.py
@@ -7,7 +7,7 @@ import os
 
 def load_config():
     # Construct the full path to the config file
-    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config.json')
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
     
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
