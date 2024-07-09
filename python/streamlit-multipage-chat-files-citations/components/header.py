29d6d0ff72081ad5d46362de56684f9634e0bb68
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-08 19:01:50 -1000

    Update header.py
    redo header with HI info

--- python/streamlit-multipage-chat-files-citations/components/header.py
+++ python/streamlit-multipage-chat-files-citations/components/header.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def create_header():
     st.image("https://graphlitplatform.blob.core.windows.net/samples/graphlit-logo.svg", width=128)
-    st.title("Graphlit Platform")
+    st.title("Hawaii Farm Data Dialog Graphlit Platform")
     st.markdown("Chat with any uploaded files, with citations.  Text extraction and OCR done with [Azure AI Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence).  Chat completion uses the [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.")
