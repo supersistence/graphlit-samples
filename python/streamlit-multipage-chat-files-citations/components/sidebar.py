13427084269762b44ea0137c2232aa507398a34c
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-08 19:19:36 -1000

    Update sidebar.py
    moved zotero

--- python/streamlit-multipage-chat-files-citations/components/sidebar.py
+++ python/streamlit-multipage-chat-files-citations/components/sidebar.py
@@ -5,12 +5,11 @@ def create_sidebar():
         st.info("""
                 ### ✅ Instructions
                 - **Step 1:** Generate Graphlit project token.
-                - **Step 2:** Browse for files to upload and ingest. Or review the [already loaded documents and datasets](https://www.zotero.org/groups/5591103/hi_farm_studies/library).
+                - **Step 2:** Browse for files to upload and ingest.
                 - **Step 3:** Enter a prompt to ask about the files using [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.
                 """)
 
         st.markdown("""
-            [Support on Discord](https://discord.gg/ygFmfjy3Qx)            
-            [API Reference](https://docs.graphlit.dev/graphlit-data-api/api-reference)     
+            [Review the documents and datasets](https://www.zotero.org/groups/5591103/hi_farm_studies/library)      
             [More information](https://www.graphlit.com)      
             """)
