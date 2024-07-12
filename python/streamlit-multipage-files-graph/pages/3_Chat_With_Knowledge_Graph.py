da0a0825bcc445425a3ba097ed591c08141d9ec5
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-12 13:31:38 -1000

    Update 3_Chat_With_Knowledge_Graph.py

--- python/streamlit-multipage-files-graph/pages/3_Chat_With_Knowledge_Graph.py
+++ python/streamlit-multipage-files-graph/pages/3_Chat_With_Knowledge_Graph.py
@@ -1,88 +1,58 @@
-import logging
 import streamlit as st
 from other import helpers, graph_helpers, client
 from components import prompt, header, sidebar, session_state
 from graphlit_api import *
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
 session_state.reset_session_state()
 sidebar.create_sidebar()
 header.create_header()
 
-def main():
-    if st.session_state['token'] is None:
-        st.info("💡 To get started, generate a token to connect to your Graphlit project.")
-    else:
-        if "messages" not in st.session_state:
+if st.session_state['token'] is None:
+    st.info("💡 To get started, generate a token to connect to your Graphlit project.")
+else:
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # render previous messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if user_prompt := st.chat_input("Ask me anything about your content.", key="chat_input"):
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
+        
+        # render user prompt
+        with st.chat_message("user"):
+            st.markdown(user_prompt)
+        
+        message, graph, error_message = helpers.run_async_task(prompt.handle_prompt, user_prompt)
+
+        if error_message is not None:
+            st.error(f"Failed to prompt conversation. {error_message}")
+        else:
+            # prompt conversation
+            with st.chat_message("assistant"):
+                st.session_state.messages.append({"role": "assistant", "content": message})
+
+                # render assistant message
+                st.markdown(message)
+
+                # render retrieved graph
+                if graph is not None:
+                    g = graph_helpers.create_pyvis_conversation_graph(graph)
+                    
+                    graph_helpers.display_pyvis_graph(g)
+
+                    # NOTE: uncomment to show the raw JSON for the graph
+                    #json = graph.model_dump_json(indent=2)
+                    #st.json(json)   
+
+    with st.form("data_feed_form"):    
+        clear_conversation = st.form_submit_button("Clear RAG conversation")
+
+        if clear_conversation:
             st.session_state.messages = []
 
-        # render previous messages
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+            helpers.run_async_task(client.clear_conversation)
 
-        if user_prompt := st.chat_input("Ask me anything about your content.", key="chat_input"):
-            st.session_state.messages.append({"role": "user", "content": user_prompt})
-            
-            # render user prompt
-            with st.chat_message("user"):
-                st.markdown(user_prompt)
-            
-            try:
-                # Log input values for debugging
-                logging.debug(f"user_prompt: {user_prompt}")
-
-                # Run the async task
-                message, graph, citations, error_message = helpers.run_async_task(prompt.handle_prompt, user_prompt)
-                
-                # Log output values for debugging
-                logging.debug(f"message: {message}, graph: {graph}, citations: {citations}, error_message: {error_message}")
-
-                if error_message is not None:
-                    st.error(f"Failed to prompt conversation. {error_message}")
-                else:
-                    # prompt conversation
-                    with st.chat_message("assistant"):
-                        st.session_state.messages.append({"role": "assistant", "content": message})
-
-                        # render assistant message
-                        st.markdown(message)
-
-                        # render retrieved graph
-                        if graph is not None:
-                            g = graph_helpers.create_pyvis_conversation_graph(graph)
-                            
-                            graph_helpers.display_pyvis_graph(g)
-
-                            # NOTE: uncomment to show the raw JSON for the graph
-                            #json = graph.model_dump_json(indent=2)
-                            #st.json(json)   
-
-                        # render citations
-                        if citations is not None:
-                            helpers.render_citations(citations)
-
-            except ValueError as e:
-                # Log the error with detailed information
-                logging.error(f"ValueError encountered: {e}", exc_info=True)
-                st.error("An error occurred while processing your request. Please try again later.")
-                
-            except Exception as e:
-                # Catch-all for other unexpected exceptions
-                logging.error(f"Unexpected error encountered: {e}", exc_info=True)
-                st.error("An unexpected error occurred. Please contact support.")
-
-        with st.form("data_feed_form"):    
-            clear_conversation = st.form_submit_button("Clear RAG conversation")
-
-            if clear_conversation:
-                st.session_state.messages = []
-
-                helpers.run_async_task(client.clear_conversation)
-
-                st.rerun()
-
-if __name__ == "__main__":
-    main()
+            st.rerun()
