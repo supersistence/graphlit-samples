3f925c46fe632f10e63930458346ecd4116671cb
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-12 23:15:00 -1000

    Update Start_Here.py
    img update

--- python/streamlit-multipage-chat-files-citations/Start_Here.py
+++ python/streamlit-multipage-chat-files-citations/Start_Here.py
@@ -97,8 +97,9 @@ with col1:
 with col2:        
     st.markdown("**Hawaii farm and agricultural research resources have been added to a database that can be queried.**")
     st.image(
-            "https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement
+            "https://images.squarespace-cdn.com/content/v1/5e3885654a153a6ef84e6c9c/1653003266864-VIGG314YMEN3YTAZDR93/SUPERSISTENCE%2Blogo.jpg
+",
+            width=100, # Manually Adjust the width of the image as per requirement
         )
 
  
