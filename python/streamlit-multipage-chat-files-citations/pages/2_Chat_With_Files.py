1839755dd7a009f3c7747503260ab8a906161d92
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-12 23:20:18 -1000

    Update 2_Chat_With_Files.py

--- python/streamlit-multipage-chat-files-citations/pages/2_Chat_With_Files.py
+++ python/streamlit-multipage-chat-files-citations/pages/2_Chat_With_Files.py
@@ -18,7 +18,7 @@ st.session_state['content_done'] = True
 if st.session_state['token'] is None:
     st.info("💡 To get started, generate a token to connect to your Graphlit project.")
 else:
-    col1, col2 = st.columns(2)
+    col1 = st.columns(1)
 
     with col1:
         if "messages" not in st.session_state:
@@ -52,35 +52,3 @@ else:
                     if citations:
                         helpers.render_citations(citations)
     
-    with col2:
-        st.markdown("**Python SDK code example:**")
-        
-        with stylable_container(
-            "codeblock",
-            """
-            code {
-                white-space: pre-wrap !important;
-                overflow-x: auto;
-                width: 100%;
-                font-size: 12px;
-            }
-            """,
-        ):
-            st.code(
-                language="python", 
-                body="""
-                from graphlit import Graphlit
-                from graphlit_api import *
-
-                # NOTE: Using LLM via `specification-id`
-                
-                input = ConversationInput(
-                    name="Conversation",
-                    specification=EntityReferenceInput(
-                        id="{specification-id}"
-                    )
-                )
-
-                response = await graphlit.client.create_conversation(input)
-                """
-            )
