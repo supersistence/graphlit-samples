9e3dadabff9b5809ca0e8cd214357507a444e357
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-23 22:19:52 -1000

    Update header.py

--- python/streamlit-multipage-chat-files-citations/components/header.py
+++ python/streamlit-multipage-chat-files-citations/components/header.py
@@ -3,4 +3,4 @@ import streamlit as st
 def create_header():
     st.image("https://raw.githubusercontent.com/supersistence/graphlit-samples/main/python/streamlit-multipage-chat-files-citations/components/hi_data_logo_dalle.png", width=128)
     st.title("Hawaii Farm Data Dialog Graphlit Platform")
-    st.markdown("Chat with any uploaded files, with citations.  Text extraction and OCR done with [Azure AI Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence).  Chat completion uses the [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.")
+    #st.markdown("Chat with any uploaded files, with citations.  Text extraction and OCR done with [Azure AI Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence).  Chat completion uses the [Anthropic](https://www.anthropic.com) Claude 3 Haiku LLM.")
