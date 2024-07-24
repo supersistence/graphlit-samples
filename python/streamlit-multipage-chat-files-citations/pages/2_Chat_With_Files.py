ce828ff760cef68e28b7183b878ff67abb6c8830
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2024-07-23 21:55:23 -1000

    Update 2_Chat_With_Files.py

--- python/streamlit-multipage-chat-files-citations/pages/2_Chat_With_Files.py
+++ python/streamlit-multipage-chat-files-citations/pages/2_Chat_With_Files.py
@@ -23,7 +23,7 @@ else:
     if "messages" not in st.session_state:
             st.session_state.messages = []
     # Render previous messages
-        for message in st.session_state.messages:
+    for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
